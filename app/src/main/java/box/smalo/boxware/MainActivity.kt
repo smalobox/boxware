1fc3b90726e2e5dc1f35712833ce083acdf8e192
ligi <ligi@ligi.de>
2018-03-28 18:07:54 +0200

    Cleanup

--- app/src/main/java/box/smalo/boxware/MainActivity.kt
+++ app/src/main/java/box/smalo/boxware/MainActivity.kt
@@ -75,19 +75,10 @@ class MainActivity : Activity() {
 
         qr_display.setQRCode("ethereum:" + keyStore.accounts[0].address.hex)
 
-        Thread(Runnable {
-            while (true) {
-
-                SystemClock.sleep(5000)
-            }
-        }).start()
-
-
         var currentNewHead = 0L
         var running = true
         var round = 1
         var lastBalance = ZERO
-
 
         Thread(Runnable {
             ethereumNode.start()
