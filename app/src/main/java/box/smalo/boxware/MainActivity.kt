2a3f6873ccaf4fc0d438631c48bc5be766c4239c
ligi <ligi@ligi.de>
2018-03-29 00:15:18 +0200

    WIP logs

--- app/src/main/java/box/smalo/boxware/MainActivity.kt
+++ app/src/main/java/box/smalo/boxware/MainActivity.kt
@@ -3,14 +3,17 @@ package box.smalo.boxware
 import android.app.Activity
 import android.os.Bundle
 import android.os.SystemClock
+import android.util.Log
 import android.view.Window
 import android.view.WindowManager
 import com.google.android.things.contrib.driver.pwmservo.Servo
 import kotlinx.android.synthetic.main.activity_main.*
 import org.ethereum.geth.*
+import org.walleth.khex.toHexString
 import java.io.File
-import java.math.BigInteger
 import java.math.BigInteger.ZERO
+
+val contractAddress = "0x2f6bd3a816f4de987b89c3dc21e1a56097f64fa3"
 
 class MainActivity : Activity() {
 
@@ -73,13 +76,14 @@ class MainActivity : Activity() {
             State.hasInitialAccount = true
         }
 
-        qr_display.setQRCode("ethereum:0xE2698bAA693aAC31Ed1Cc12A8c7F1234aE405A6e@4/rent?value=0.001e18")
+        qr_display.setQRCode("ethereum:$contractAddress@4/rent?value=0.001e18&gas=200000")
         //qr_display.setQRCode("ethereum:" + keyStore.accounts[0].address.hex)
 
         var currentNewHead = 0L
         var running = true
         var round = 1
         var lastBalance = ZERO
+
 
         Thread(Runnable {
             ethereumNode.start()
@@ -96,6 +100,26 @@ class MainActivity : Activity() {
 
             var lastBlock = ethereumNode.ethereumClient.syncProgress(ethereumContext)?.highestBlock
 
+            class MyHandler : FilterLogsHandler {
+                override fun onFilterLogs(p0: org.ethereum.geth.Log?) {
+                    Log.i("Tag", "GotLog1" + p0.toString())
+                    Log.i("Tag", "GotLog-h" + p0!!.address.hex)
+                    Log.i("Tag", "GotLog-d" + p0.data.toList().toHexString())
+                    (0..p0.topics.size()).forEach {
+                        Log.i("Tag", "GotLog-t" + it + p0.topics[it].hex)
+                    }
+                }
+
+                override fun onError(p0: String?) {
+                }
+            }
+
+            val query = FilterQuery().apply {
+                addresses = Geth.newAddressesEmpty().apply { append(Geth.newAddressFromHex(contractAddress)) }
+            }
+
+            ethereumNode.ethereumClient.subscribeFilterLogs(ethereumContext, query, MyHandler(), 0L)
+
             while (running) {
                 SystemClock.sleep(3000)
                 val currentBlock = ethereumNode.ethereumClient.syncProgress(ethereumContext)?.currentBlock
@@ -106,17 +130,17 @@ class MainActivity : Activity() {
 
                 round++
 
-                val balanceBigInt = ethereumNode.ethereumClient.getBalanceAt(ethereumContext, keyStore.accounts[0].address, currentNewHead)
-                val balance = BigInteger(balanceBigInt.string())
+                //val balanceBigInt = ethereumNode.ethereumClient.getBalanceAt(ethereumContext, keyStore.accounts[0].address, currentNewHead)
+                //val balance = BigInteger(balanceBigInt.string())
 
-                if (lastBalance != balance) {
+                /*if (lastBalance != balance) {
                     toggleServo()
                     lastBalance = balance
-                }
+                }*/
 
                 runOnUiThread {
                     val text = "IP:" + getIPAddress(true) + "\nBlock: " + currentNewHead + "/" + (highestBlock
-                            ?: currentNewHead) + "\nPeers:" + ethereumNode.peersInfo.size() + "\nBalance:" + balance + "\nround:$round"
+                            ?: currentNewHead) + "\nPeers:" + ethereumNode.peersInfo.size() + "\nround:$round"
                     node_text.text = text
                 }
             }
