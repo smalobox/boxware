695f09c729280cfe4e62296bff479a6a78f1b76a
ligi <ligi@ligi.de>
2018-03-29 05:44:44 +0200

    WIP

--- app/src/main/java/box/smalo/boxware/MainActivity.kt
+++ app/src/main/java/box/smalo/boxware/MainActivity.kt
@@ -16,9 +16,9 @@ import java.io.File
 import java.math.BigInteger
 import java.math.BigInteger.ZERO
 
-val TOPIC_RENTED ="Rented(address,address,uint256,uint256)".toByteArray().keccak().toHexString()
-val TOPIC_OPENED ="Opened(address,address)".toByteArray().keccak().toHexString()
-val TOPIC_CLOSED ="Returned(address,address)".toByteArray().keccak().toHexString()
+val TOPIC_RENTED = "Rented(address,address,uint256,uint256)".toByteArray().keccak().toHexString()
+val TOPIC_OPENED = "Opened(address,address)".toByteArray().keccak().toHexString()
+val TOPIC_CLOSED = "Returned(address,address)".toByteArray().keccak().toHexString()
 
 val contractAddress = "0x2f6bd3a816f4de987b89c3dc21e1a56097f64fa3"
 
@@ -27,7 +27,9 @@ class MainActivity : Activity() {
     private val keyStoreFile by lazy { File(filesDir, "keystore") }
     private val keyStore by lazy { KeyStore(keyStoreFile.absolutePath, Geth.LightScryptN, Geth.LightScryptP) }
 
-    private var rentedUntil :Long? = null
+    private var rentedUntil: Long? = null
+
+    var foo = 1
 
     private enum class Network {
         RINKEBY, MAIN, ROPSTEN
@@ -77,16 +79,16 @@ class MainActivity : Activity() {
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         window.setFlags(FLAG_FULLSCREEN, FLAG_FULLSCREEN);
 
-        Log.i("GotLog", "RentSig:"+TOPIC_RENTED)
-        Log.i("GotLog", "OpenSig:"+ TOPIC_OPENED)
-        Log.i("GotLog", "CloseSig:"+ TOPIC_CLOSED)
+        Log.i("GotLog", "RentSig:" + TOPIC_RENTED)
+        Log.i("GotLog", "OpenSig:" + TOPIC_OPENED)
+        Log.i("GotLog", "CloseSig:" + TOPIC_CLOSED)
 
         setContentView(R.layout.activity_main)
 
         main_text.text = getIPAddress(true)
 
         if (startNode) {
-            main_text.text = "starting:"+getIPAddress(true)
+            main_text.text = "starting:" + getIPAddress(true)
             if (!State.hasInitialAccount) {
                 keyStore.newAccount(DEFAULT_PASSWORD)
                 State.hasInitialAccount = true
@@ -99,7 +101,6 @@ class MainActivity : Activity() {
             var running = true
             var round = 1
             var lastBalance = ZERO
-
 
             Thread(Runnable {
                 ethereumNode.start()
@@ -116,20 +117,26 @@ class MainActivity : Activity() {
 
                 var lastBlock = ethereumNode.ethereumClient.syncProgress(ethereumContext)?.highestBlock
 
+
                 class MyHandler : FilterLogsHandler {
                     override fun onFilterLogs(p0: org.ethereum.geth.Log?) {
-                        Log.i("Tag", "GotLog1" + p0.toString())
-                        Log.i("Tag", "GotLog-h" + p0!!.address.hex)
-                        Log.i("Tag", "GotLog-d" + p0.data.toList().toHexString())
-                        (0..p0.topics.size()).forEach {
-                            if (TOPIC_RENTED.toLowerCase()== p0.topics[it].hex) {
-                                val toNoPrefixHexString = BigInteger(p0.data.toList().subList(0, 32).toNoPrefixHexString(),16)
-                                Log.i("Tag", "GotLog-RENTED" + toNoPrefixHexString + "mins")
+                        try {
+                            synchronized(foo) {
+                                Log.i("Tag", "GotLog1" + p0.toString())
+                                Log.i("Tag", "GotLog-h" + p0!!.address.hex)
+                                Log.i("Tag", "GotLog-d" + p0.data.toList().toHexString())
+                                (0..p0.topics.size()).forEach {
+                                    if (TOPIC_RENTED.toLowerCase() == p0.topics[it].hex) {
+                                        val toNoPrefixHexString = BigInteger(p0.data.toList().subList(0, 32).toNoPrefixHexString(), 16)
+                                        Log.i("Tag", "GotLog-RENTED" + toNoPrefixHexString + "mins")
 
-                                rentedUntil = System.currentTimeMillis() + toNoPrefixHexString.toLong()*60*1000
+                                        rentedUntil = System.currentTimeMillis() + toNoPrefixHexString.toLong() * 60 * 1000
 
+                                    }
+                                    Log.i("Tag", "GotLog-t" + it + p0.topics[it].hex)
+                                }
                             }
-                            Log.i("Tag", "GotLog-t" + it + p0.topics[it].hex)
+                        } catch (e: Exception) {
                         }
                     }
 
@@ -137,11 +144,26 @@ class MainActivity : Activity() {
                     }
                 }
 
+
                 val query = FilterQuery().apply {
                     addresses = Geth.newAddressesEmpty().apply { append(Geth.newAddressFromHex(contractAddress)) }
+                    fromBlock = BigInt(State.lastProcessedBlock)
+                    toBlock = BigInt(currentNewHead)
                 }
 
+
                 ethereumNode.ethereumClient.subscribeFilterLogs(ethereumContext, query, MyHandler(), 0L)
+                /*try {
+                    val filterLogs = ethereumNode.ethereumClient.filterLogs(ethereumContext, query)
+                    if (filterLogs.size() > 0) {
+                        Log.i("GotLog", "size:" + filterLogs.size())
+                    }
+                    State.lastProcessedBlock = currentNewHead
+
+                } catch (e: Exception) {
+                    Log.i("GotLog", "m:" + e.message)
+                }
+                */
 
                 while (running) {
                     SystemClock.sleep(1000)
@@ -163,15 +185,15 @@ class MainActivity : Activity() {
 
                     runOnUiThread {
 
-                        val secondsLeft=((rentedUntil?:0)-System.currentTimeMillis())/1000
-                        var text ="Block: " + currentNewHead + "/" + (highestBlock
+                        val secondsLeft = secondsLeft()
+                        var text = "Block: " + currentNewHead + "/" + (highestBlock
                                 ?: currentNewHead) + "\nPeers:" + ethereumNode.peersInfo.size() + " - Tick:$round\nid=" + BuildConfig.APPLICATION_ID
-                        text+="\nrented: " + if (secondsLeft>0) {
-                            secondsLeft.toString()+"s"
+                        text += "\nrented: " + if (secondsLeft > 0) {
+                            secondsLeft.toString() + "s"
                         } else {
                             "no"
                         }
-
+                        text += "\nLP: " + State.lastProcessedBlock
                         node_text.text = text
                     }
                 }
@@ -179,6 +201,10 @@ class MainActivity : Activity() {
                 ethereumNode.stop()
             }).start()
         }
+    }
+
+    private fun secondsLeft() = synchronized(foo) {
+        ((rentedUntil ?: 0) - System.currentTimeMillis()) / 1000
     }
 
     private fun toggleServo() {
--- app/src/main/java/box/smalo/boxware/State.kt
+++ app/src/main/java/box/smalo/boxware/State.kt
@@ -4,4 +4,5 @@ import com.chibatching.kotpref.KotprefModel
 
 object State : KotprefModel() {
     var hasInitialAccount by booleanPref(default = false)
+    var lastProcessedBlock by longPref(default = 2014998L)
 }
